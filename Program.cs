e86017136936d08199bfc388fd0557cf0126f8ae
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-17 15:01:48 +0100

    Aggiunge funzione stampa array e funzione quadrato

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
 ﻿
-Console.WriteLine("Hello, World!");
+void StampaArray(int[] array)
+{
+    Console.WriteLine(arrayToString(array));
+}
+
+string arrayToString(int[] array)
+{
+    var result = "[";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i].ToString();
+
+        if (i < array.Length - 1)
+        {
+            result += ", ";
+        }
+    }
+
+    result += "]";
+
+    return result;
+}
+
+int Quadrato(int numero)
+{
+    return numero * numero;
+}
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    //preso un array di numeri interi, restituisca un nuovo array con tutti gli elementi elevati quadrato.
+    //Attenzione: è importante restituire un nuovo array, e non modificare l’array come parametro della funzione! 
+    //Vi ricordate perchè? Pensateci (vedi slide)
+    return null;
+}
+
+int sommaElementiArray(int[] array)
+{
+    //preso un array di numeri interi, restituisca la somma totale di tutti gli elementi dell’array.
+    return 0;
+}
