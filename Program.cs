ba08c83c0a4f2cfe051c2e039f89decf7df4d61f
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-03-17 16:56:17 +0100

    Aggiunge uso funzioni

--- Program.cs
+++ Program.cs
@@ -1,4 +1,29 @@
 ﻿
+//Una volta completate queste funzioni di utilità di base, e dato il seguente array di numeri [2, 6, 7, 5, 3, 9] già dichiarato nel vostro codice, 
+//si vogliono utilizzare le funzioni per:
+
+int[] numbers = {2, 6, 7, 5, 3, 9};
+
+Console.WriteLine("-Stampare l’array di numeri fornito a video.");
+StampaArray(numbers);
+
+Console.WriteLine("-Stampare l’array di numeri fornito a video, dove ogni numero è stato prima elevato al quadrato."); //(Verificare che l’array originale non sia stato modificato quindi ristampare nuovamente l’array originale e verificare che sia rimasto [2, 6, 7, 5, 3, 9])
+var arrayElevato = ElevaArrayAlQuadrato(numbers); 
+StampaArray(arrayElevato);
+Console.WriteLine();
+
+Console.WriteLine("-Stampare la somma di tutti i numeri.");
+Console.WriteLine(sommaElementiArray(numbers));
+Console.WriteLine();
+
+Console.WriteLine("-Stampare la somma di tutti i numeri elevati al quadrato.");
+var arrayElevato2 = ElevaArrayAlQuadrato(numbers);
+var arraySommato = sommaElementiArray(arrayElevato2);
+Console.WriteLine(arraySommato);
+
+
+/*--------------------------------------------------------------FUNZIONI----------------------------------------------------------------------------------------*/
+
 void StampaArray(int[] array)
 {
     Console.WriteLine(arrayToString(array));
